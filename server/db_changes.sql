c256f98d95095627eff938a76b9d9b8406eb120a
KamleshNaidu07 <kamleshnaidu07@gmail.com>
2023-05-19 21:07:58 +0530

    Added appointments table

--- server/db_changes.sql
+++ server/db_changes.sql
@@ -25,4 +25,14 @@ CREATE TABLE doctors (
 );
 
 
--- 
+-- for appointment table
+CREATE TABLE appointments(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    doctor_id INT NOT NULL,
+    patient_id INT NOT NULL,
+    appointment_date DATETIME NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY(doctor_id) REFERENCES doctors(id) ON DELETE CASCADE,
+    FOREIGN KEY(patient_id) REFERENCES patients(id) ON DELETE CASCADE
+);
