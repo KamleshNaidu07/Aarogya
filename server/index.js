12ba639e0f98d9180e3aef6b503e130112770326
KamleshNaidu07 <kamleshnaidu07@gmail.com>
2023-05-19 21:08:44 +0530

    Added routes for appointment api

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const patientRoutes = require('./routes/patientRoutes');
 const doctorRoutes = require('./routes/doctorRoutes');
+const appointmentRoutes = require('./routes/appointmentRoutes');
 
 const app = express();
 
@@ -11,6 +12,9 @@ app.use(bodyParser.json());
 // Routes
 app.use('/api/patients', patientRoutes);
 app.use('/api/doctors', doctorRoutes);
+app.use('/api/appointments', appointmentRoutes);
+
+
 
 
 // Root endpoint
